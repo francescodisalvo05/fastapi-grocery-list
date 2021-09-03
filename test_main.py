59e8560ee6f79a6cb765bdc7e6335bf88f045934
Francesco Di Salvo <francesco.disalvo99@gmail.com>
2021-09-03 20:00:54 +0200

    Fixed tests

--- test_main.py
+++ test_main.py
@@ -3,48 +3,53 @@ from app.app import app
 
 client = TestClient(app)
 
+def test_greetings():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Welcome":"Thank you for using our service!"}
+
 
 def test_read_items():
-    response = client.get("/todo")
+    response = client.get("/list")
 
     assert response.status_code == 200
     assert response.json() == {
                             "data": [
-                                {"id": 1,"activity": "read"},
-                                {"id": 2,"activity": "eat"}
+                                {"item" : "bread", "qty" : 1},
+                                {"item" : "milk", "qty" : 2}
                             ]}
 
 def test_create_inexistent_item():
-    response = client.post("/create", json={"id" : 10000000, "activity" : "testing"})
+    response = client.post("/create", json={"item" : "new_item", "qty" : 10})
     assert response.status_code == 201
-    assert response.json() == {"data" : "Added correctly!"}
+    assert response.json() == {"data" : "new_item added correctly!"}
     
 
 def test_create_existent_item():
-    response = client.post("/create", json={"id" : 1, "activity" : "testing"})
+    response = client.post("/create", json={"item" : "bread", "qty" : 3})
     assert response.status_code == 400
-    assert response.json() == {"detail" : "Item already present!"}
+    assert response.json() == {"detail" : "bread already present!"}
 
 
 def test_update_inexistent_item():
-    response = client.put("/update?id=10000", json={"activity" : "testing"})
+    response = client.put("/update?item_name=fake_item&item_quantity=55")
     assert response.status_code == 404
-    assert response.json() == {"detail" : "Item not found!"}
+    assert response.json() == {"detail" : "fake_item not found!"}
     
 
 def test_update_existent_item():
-    response = client.put("/update?id=1", json={"activity" : "testing"})
+    response = client.put("/update?item_name=milk&item_quantity=55")
     assert response.status_code == 200
-    assert response.json() == {"data" : "Updated correctly!"}
+    assert response.json() == {"data" : "milk correctly updated!"}
 
 
 def test_delete_inexistent_item():
-    response = client.delete("/delete/100000000")
+    response = client.delete("/delete/fake_name")
     assert response.status_code == 404
-    assert response.json() == {"detail" : "Item not found!"}
+    assert response.json() == {"detail" : "fake_name not found!"}
     
 
 def test_delete_existent_item():
-    response = client.delete("/delete/1")
+    response = client.delete("/delete/milk")
     assert response.status_code == 200
-    assert response.json() == {'data' : "Deleted!"}
+    assert response.json() == {'data' : "milk correctly deleted!"}
